5080aa183dada286c3c0ab7ab869f6dc025fe77c
Rafi <mrafianshory@gmail.com>
2025-05-24 14:34:38 +0700

    Differentiate the UI from the user roles

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,13 @@
+@if (Auth::check() && Auth::user()->roles === 'admin')
 <x-layouts.app.sidebar :title="$title ?? null">
     <flux:main>
         {{ $slot }}
     </flux:main>
 </x-layouts.app.sidebar>
+@elseif (Auth::check() && (Auth::user()->roles === 'siswa' || Auth::user()->roles === 'guru'))
+<x-layouts.app.header :title="$title ?? null">
+    <flux:main>
+        {{ $slot }}
+    </flux:main>
+</x-layouts.app.header>
+@endif
