35cb15f84edf01f45dbeed1455bf82d1b1671312
Rafi <mrafianshory@gmail.com>
2025-05-25 19:48:35 +0700

    Added the industry and teachers page for siswa

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,25 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+// Page Admin
 Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
-    Route::get('/admin/dashboard', \App\Livewire\Admin\Dashboard::class)->name('admin-dashboard');
-    Route::get('/admin/users', \App\Livewire\Admin\Users::class)->name('users');
-    Route::get('/admin/internships', \App\Livewire\Admin\Internships::class)->name('internships');
-
-    Route::get('/admin/students', \App\Livewire\Admin\Students::class)->name('students');
-    Route::get('/admin/teachers', \App\Livewire\Admin\Teachers::class)->name('teachers');
-    Route::get('/admin/industries', \App\Livewire\Admin\Industries::class)->name('industries');
+    Route::get('/admin/dashboard', \App\Livewire\Admin\Dashboard::class)->name('admin.dashboard');
+    Route::get('/admin/users', \App\Livewire\Admin\Users::class)->name('admin.users');
+    Route::get('/admin/internships', \App\Livewire\Admin\Internships::class)->name('admin.internships');
+    Route::get('/admin/students', \App\Livewire\Admin\Students::class)->name('admin.students');
+    Route::get('/admin/teachers', \App\Livewire\Admin\Teachers::class)->name('admin.teachers');
+    Route::get('/admin/industries', \App\Livewire\Admin\Industries::class)->name('admin.industries');
 });
 
+// Page Siswa
 Route::middleware(['auth', 'verified', 'role:siswa'])->group(function () {
-    Route::get('/siswa/dashboard', \App\Livewire\Siswa\Dashboard::class)->name('siswa-dashboard');
+    Route::get('/siswa/dashboard', \App\Livewire\Siswa\Dashboard::class)->name('siswa.dashboard');
+    Route::get('/siswa/teachers', \App\Livewire\Siswa\Teachers::class)->name('siswa.teachers');
+    Route::get('/siswa/industries', \App\Livewire\Siswa\Industries::class)->name('siswa.industries');
 });
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
-
     Route::get('/profile', Profile::class)->name('settings.profile');
     Route::get('/password', Password::class)->name('settings.password');
     Route::get('/appearance', Appearance::class)->name('settings.appearance');
